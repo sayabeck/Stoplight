2ccf906943853025c4ed875c5757f8214951eceb
sayabeck <sayabeck@mail.ru>
2022-03-10 20:16:18 +0600

    Completed

--- Stoplight/ViewController.swift
+++ Stoplight/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var button: UIButton!
+    
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        button.layer.cornerRadius = 10
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.backgroundColor = .red
+        yellowLight.backgroundColor = .yellow
+        greenLight.backgroundColor = .green
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
     }
 
-
+    @IBAction func buttonPressed() {
+        
+        button.setTitle("Next", for: .normal)
+        
+        if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        } else if yellowLight.alpha == 1 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        } else if greenLight.alpha == 1 {
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        } else {
+            redLight.alpha = 1
+        }
+    }
+    
 }
 
